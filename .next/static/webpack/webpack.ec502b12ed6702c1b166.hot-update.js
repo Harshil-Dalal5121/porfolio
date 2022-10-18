c7937446cd4d1074fea51e7081f44b0dac723f6c
Harshil Dalal <harshil.190410116006@svitvasad.ac.in>
2022-10-18 20:30:48 +0530

    Footer added

--- .next/static/webpack/webpack.ec502b12ed6702c1b166.hot-update.js
+++ .next/static/webpack/webpack.ec502b12ed6702c1b166.hot-update.js
@@ -4,7 +4,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "85e3f72a0057f3bc555b"; }
+/******/ 	__webpack_require__.h = function() { return "be854e1ec5ddd46d22b0"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
@@ -20,4 +20,4 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 	};
 /******/ }}
 );
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCIsIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9jb21wYXQiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7O1VBQUEsb0NBQW9DLCtCQUErQixFOzs7Ozs7U0NFbkU7U0FDQTtTQUNBO1NBQ0E7U0FDQTtTQUNBO1NBQ0E7U0FDQTtTQUNBLEMiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay5lYzUwMmIxMmVkNjcwMmMxYjE2Ni5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjg1ZTNmNzJhMDA1N2YzYmM1NTViXCI7IH0iLCJcblxuLy8gbm9vcCBmbnMgdG8gcHJldmVudCBydW50aW1lIGVycm9ycyBkdXJpbmcgaW5pdGlhbGl6YXRpb25cbmlmICh0eXBlb2Ygc2VsZiAhPT0gXCJ1bmRlZmluZWRcIikge1xuXHRzZWxmLiRSZWZyZXNoUmVnJCA9IGZ1bmN0aW9uICgpIHt9O1xuXHRzZWxmLiRSZWZyZXNoU2lnJCA9IGZ1bmN0aW9uICgpIHtcblx0XHRyZXR1cm4gZnVuY3Rpb24gKHR5cGUpIHtcblx0XHRcdHJldHVybiB0eXBlO1xuXHRcdH07XG5cdH07XG59Il0sInNvdXJjZVJvb3QiOiIifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCIsIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9jb21wYXQiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7O1VBQUEsb0NBQW9DLCtCQUErQixFOzs7Ozs7U0NFbkU7U0FDQTtTQUNBO1NBQ0E7U0FDQTtTQUNBO1NBQ0E7U0FDQTtTQUNBLEMiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay5lYzUwMmIxMmVkNjcwMmMxYjE2Ni5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcImJlODU0ZTFlYzVkZGQ0NmQyMmIwXCI7IH0iLCJcblxuLy8gbm9vcCBmbnMgdG8gcHJldmVudCBydW50aW1lIGVycm9ycyBkdXJpbmcgaW5pdGlhbGl6YXRpb25cbmlmICh0eXBlb2Ygc2VsZiAhPT0gXCJ1bmRlZmluZWRcIikge1xuXHRzZWxmLiRSZWZyZXNoUmVnJCA9IGZ1bmN0aW9uICgpIHt9O1xuXHRzZWxmLiRSZWZyZXNoU2lnJCA9IGZ1bmN0aW9uICgpIHtcblx0XHRyZXR1cm4gZnVuY3Rpb24gKHR5cGUpIHtcblx0XHRcdHJldHVybiB0eXBlO1xuXHRcdH07XG5cdH07XG59Il0sInNvdXJjZVJvb3QiOiIifQ==
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,33 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+   
+      <LinkList>
+      <LinkColumn>
+      <LinkTitle>Call</LinkTitle>
+      <LinkItem href="phone:+91 9737115121">+91 9737115121</LinkItem>
+      </LinkColumn>
+      <LinkColumn>
+      <LinkTitle>Email</LinkTitle>
+      <LinkItem href="mailto:harshil.190410116006">harshil.190410116006@gmail.com</LinkItem>
+      </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>A smart working developer</Slogan>
+        </CompanyContainer>
+        <SocialIcons href="/">
+      <AiFillGithub size="3rem" />  
+      </SocialIcons>
+      <SocialIcons href="/">
+      <AiFillLinkedin size="3rem" />  
+      </SocialIcons>
+      <SocialIcons href="/">
+      <AiFillInstagram size="3rem" />  
+      </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
