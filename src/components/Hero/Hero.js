f088026f38bd096f5566efdc0472bc551697ee56
Harshil Dalal <harshil.190410116006@svitvasad.ac.in>
2022-10-18 11:51:09 +0530

    Hero element Started

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import resume from './resume_pdf.pdf';
 
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
