3b02f4cdbb66a21324b9042286fc3330712bb497
Harshil Dalal <harshil.190410116006@svitvasad.ac.in>
2022-10-18 11:41:42 +0530

    Resume Added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,21 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
-);
+const Hero = (props) => {
+  return (
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome to my <br />
+          My Portfolio
+        </SectionTitle>
+        <SectionText>
+          I am a Web Developer eager to learn new things..💻
+        </SectionText>
+        <Button onclick={()=> window.location='resume_pdf.pdf'}>My Resume</Button>
+      </LeftSection>
+    </Section>
+  );
+};
 
 export default Hero;
